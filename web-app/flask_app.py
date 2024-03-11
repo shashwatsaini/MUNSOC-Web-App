04ae06259d8c70017999500a1f2a02ed0376a158
Shashwat Saini <125039068+shashwatsaini@users.noreply.github.com>
2024-03-12 01:16:28 +0530

    Update flask_app.py

--- web-app/flask_app.py
+++ web-app/flask_app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask import redirect, url_for, render_template
-from application.config import LocalDevelopmentConfig
+# from application.config import LocalDevelopmentConfig
 
 app = None
 
@@ -12,7 +12,20 @@ def create_app():
 	return app
 
 app = create_app()
-from application.controllers import *
+# from application.controllers import *
+
+@app.route('/')
+def home():
+	return render_template('home.html')
+
+@app.route('/events')
+def events():
+	return render_template('events.html')
+
+@app.route('/the-secretariat')
+def secretariat():
+	return render_template('secretariat.html')
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=5000)
+	# app.run(host='0.0.0.0', port=5000)
+	app.run()

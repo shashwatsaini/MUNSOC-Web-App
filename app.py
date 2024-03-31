79e2a2b4247984f9082850932b9f5ec2c1f21c4a
Shashwat Saini <125039068+shashwatsaini@users.noreply.github.com>
2024-03-31 23:41:29 +0530

    Added Google Drive Synchronization

--- app.py
+++ app.py
@@ -3,10 +3,12 @@ from flask import Flask
 from flask import redirect, url_for, render_template
 from application.config import LocalDevelopmentConfig
 from application.database import db
+from application.models import Teams
 
 current_dir = os.path.abspath(os.path.dirname(__file__))
 app = None
 
+# Creating the web app
 def create_app():
 	app = Flask(__name__, template_folder='templates')
 	app.config.from_object(LocalDevelopmentConfig)
@@ -14,10 +16,12 @@ def create_app():
 	db.init_app(app)
 
 	app.app_context().push()
+	db.create_all()
 	return app
 
 app = create_app()
 from application.controllers import *
 
 if __name__ == '__main__':
+	drive_service = auth('gcp_key.json')
 	app.run(host='0.0.0.0', port=5000)
--- application/controllers.py
+++ application/controllers.py
@@ -1,11 +1,19 @@
+import os
 from flask import current_app as app
 from flask import Flask, request
 from flask import render_template, request, redirect, url_for
 from application.database import db
 from application.models import Teams
+import pandas as pd
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaFileUpload
+
+drive_service = None
 
 @app.route('/')
 def home():
+	synchronize_drive('teams.csv')
 	return render_template('home.html')
 
 @app.route('/events')
@@ -18,6 +26,8 @@ def secretariat():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+	global drive_service
+
 	if request.method == 'GET':
 		return render_template('register.html', registered=False)
 	else:
@@ -38,9 +48,90 @@ def register():
 
 			db.session.add(team)
 			db.session.commit()
+
+			data = Teams.query.all()
+			data_dicts = [{'id': item.id, 'email': item.email, 'name': item.name, 'committee_pref': item.committee_pref,
+			'size': item.size, 'member_name_1': item.member_name_1, 'member_name_2': item.member_name_2,
+			'member_name_3': item.member_name_3, 'member_name_4': item.member_name_4} for item in data]
+			df = pd.DataFrame(data_dicts)
+			df.to_csv('teams.csv')
+
+			synchronize_drive('teams.csv')
+
 			return render_template('register.html', registered=True)
 
 		except Exception as e:
 			print(e)
 			return redirect('/')
+
+# Authentication for GCP
+def auth(key):
+	global drive_service
+
+	SCOPES = ['https://www.googleapis.com/auth/drive']
+	SERVICE_ACCOUNT_FILE = key
+
+	credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE)
+	drive_service = build('drive', 'v3', credentials=credentials)
+
+	return drive_service
+
+# Get or create folder in GDrive
+def get_or_create_folder(folder_name):
+    # Search for the folder by name
+    response = drive_service.files().list(q=f"name='{folder_name}' and mimeType='application/vnd.google-apps.folder'",
+      spaces='drive',
+      fields='files(id)').execute()
+    folders = response.get('files', [])
+
+    if folders:
+        folder_id = folders[0]['id']
+        add_folder_permission(folder_id)
+        print(f"Folder '{folder_name}' found with ID: {folder_id}")
+        return folder_id
+    else:
+    	# Create the folder
+        print(f"No folder named '{folder_name}' found. Creating...")
+        file_metadata = {
+            'name': folder_name,
+            'mimeType': 'application/vnd.google-apps.folder'
+        }
+        folder = drive_service.files().create(body=file_metadata,
+           fields='id').execute()
+        folder_id = folder.get('id')
+        add_folder_permission(folder_id)
+        print(f"Folder '{folder_name}' created with ID: {folder_id}")
+        return folder_id
+
+# Add folder permissions to view the folder in GDrive UI
+def add_folder_permission(folder_id, role='writer'):
+    permission = {
+        'type': 'anyone',
+        'role': role
+    }
+
+    drive_service.permissions().create(fileId=folder_id, body=permission).execute()
+
+# Synchronize the csv format of the database with GDrive
+def synchronize_drive(file_path, folder_name='MUNSOC Web App'):
+    folder_id = get_or_create_folder(folder_name)
+    
+    # Search for the existing file in the folder
+    response = drive_service.files().list(q=f"name='{os.path.basename(file_path)}' and '{folder_id}' in parents",
+                                          fields='files(id)').execute()
+    files = response.get('files', [])
+    
+    if files:
+        file_id = files[0]['id']
+        # Update the existing file
+        media = MediaFileUpload(file_path, mimetype='application/octet-stream', resumable=True)      
+        file = drive_service.files().update(fileId=file_id, media_body=media).execute()
+        print(f"File '{os.path.basename(file_path)}' updated in folder '{folder_name}'")
+    else:
+        # Create the file if it doesn't exist
+        file_metadata = {'name': os.path.basename(file_path), 'parents': [folder_id]}
+        media = MediaFileUpload(file_path, mimetype='application/octet-stream', resumable=True)      
+        file = drive_service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+        print(f"File '{os.path.basename(file_path)}' created in folder '{folder_name}'")
+
 
